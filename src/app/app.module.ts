f76ff5093f5fa63f10b81ea861c424021686e23f
YouheiNozaki <youhey.hey0505@gmail.com>
2021-01-25 23:19:30 +0900

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from "./app.component";
 import { TopBarComponent } from "./top-bar/top-bar.component";
 import { ProductListComponent } from "./product-list/product-list.component";
 import { ProductAlertsComponent } from "./product-alerts/product-alerts.component";
+import { ProductsDetailsComponent } from './products-details/products-details.component';
 
 @NgModule({
   imports: [
@@ -18,7 +19,8 @@ import { ProductAlertsComponent } from "./product-alerts/product-alerts.componen
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    ProductsDetailsComponent
   ],
   bootstrap: [AppComponent]
 })
