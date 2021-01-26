4206121ceb4a8bc275caefddc69a9fc85d62ae72
YouheiNozaki <youhey.hey0505@gmail.com>
2021-01-27 00:13:58 +0900

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
 import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { TopBarComponent } from "./top-bar/top-bar.component";
@@ -10,10 +11,12 @@ import { ProductAlertsComponent } from "./product-alerts/product-alerts.componen
 import { ProductDetailsComponent } from "./product-details/product-details.component";
 import { CartService } from "./cart.service";
 import { CartComponent } from "./cart/cart.component";
+import { ShippingComponent } from "./shipping/shipping.component";
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: "", component: ProductListComponent },
@@ -24,6 +27,10 @@ import { CartComponent } from "./cart/cart.component";
       {
         path: "cart",
         component: CartComponent
+      },
+      {
+        path: "shipping",
+        component: ShippingComponent
       }
     ])
   ],
@@ -33,7 +40,8 @@ import { CartComponent } from "./cart/cart.component";
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
-    CartComponent
+    CartComponent,
+    ShippingComponent
   ],
   bootstrap: [AppComponent],
   providers: [CartService]
